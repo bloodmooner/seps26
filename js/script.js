4db3d60f1e0509aa4b00460dd208c6c2c2b4bf96
Sergey Goncharuk <fleshed914@gmail.com>
2022-09-16 23:42:56 +0300

    feat: add product, breadcrumbs, catalog, category styles & modal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 
 $(document).ready(function(){
 
- 
+    $('.js-open-modal-order').on('click', function(){
+        $('.overlays').addClass('overlays_active');
+        $('.modal_order').addClass('modal_order-active');
+    });
+
+    $('.modal__close').on('click', function(){
+        $('.overlays').removeClass('overlays_active');
+        $('.modal_order').removeClass('modal_order-active');
+    });
   
 
 });
