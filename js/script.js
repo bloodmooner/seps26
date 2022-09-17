a892bec004c1f9af0960f60be44ba4211c306262
Sergey Goncharuk <fleshed914@gmail.com>
2022-09-17 22:02:56 +0300

    feat: add tabs to product

--- js/script.js
+++ js/script.js
@@ -12,5 +12,10 @@ $(document).ready(function(){
     });
   
 
+    $(".wrapper .tab").click(function() {
+        $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
 });
 
