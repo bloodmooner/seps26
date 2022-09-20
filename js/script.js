4c5127570a50164d1369d6bb73a07a2dfb4b546e
Sergey Goncharuk <fleshed914@gmail.com>
2022-09-20 23:29:04 +0300

    feat: add slick to tabs

--- js/script.js
+++ js/script.js
@@ -12,10 +12,61 @@ $(document).ready(function(){
     });
   
 
-    $(".wrapper .tab").click(function() {
+    /* $(".wrapper .tab").click(function() {
         $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
         $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active"); */
+
+    
+
+    /* $(".slick-slide").click(function() {
+        alert('test');
+        
+    }); */
+
+
+    $('body').on('click', '.slick-slide', function () {
+        alert('click working');
+        $(this).find(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active")
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
     }).eq(0).addClass("active");
+
+
+    $('.tabs').slick({
+        slidesToShow: 3,
+        arrows: true,
+        infinite: false,
+        nextArrow: $(document).find('.related__arrow_left'),
+        prevArrow: $(document).find('.related__arrow_right'),
+        responsive: [
+            {
+                breakpoint: 1290,
+                settings: {
+                  slidesToShow: 3,
+                  dots: false,
+                  arrows: true
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                  slidesToShow: 2,
+                  dots: false,
+                  arrows: true
+                }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 2,
+                variableWidth: true,
+                dots: false,
+                arrows: false
+              }
+          },
+          
+        ]
+      });
 
 });
 
