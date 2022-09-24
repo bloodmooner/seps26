08110e5f06f4d8a8d0037f15527758ddffdaaa7a
Sergey Goncharuk <fleshed914@gmail.com>
2022-09-24 23:15:54 +0300

    feat: add mobile catalog

--- js/script.js
+++ js/script.js
@@ -10,6 +10,30 @@ $(document).ready(function(){
         $('.overlays').removeClass('overlays_active');
         $('.modal_order').removeClass('modal_order-active');
     });
+
+
+     // Mobile Menu START
+
+     let closeMenuButton = $(".header-mobile__close");
+     let openMenuButton = $(".burger");
+     let menuContainer = $(".header-mobile");
+     let menuLinks = $(".header-mobile__link");
+   
+   
+     openMenuButton.click(function() {
+       menuContainer.addClass("header-mobile_active");
+     });
+   
+     closeMenuButton.click(function() {
+       menuContainer.removeClass("header-mobile_active");
+     });
+   
+     menuLinks.click(function() {
+       menuContainer.removeClass("header-mobile_active");
+     });
+ 
+ 
+     // Mobile Menu END
   
     /* $(".wrapper .tab").click(function() {
         $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
